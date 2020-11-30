dd13004c62e1e038a2e640204fbe12030d47575c
nandanchitale <nandanchitale@gmail.com>
2020-11-30 23:28:11 +0530

    Adding LinkedIn scrapping(Work in progress)

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-# def getPosition():
-#     position = input("Enter Position(E.g. Software Developer) : ")
-#     return position
+# Method to get job description
 
 
 def getDescription():
@@ -11,22 +9,83 @@ def getDescription():
     description.replace(" ", "+")
     return description
 
+# Method to get job location
+
 
 def getLocation():
-    location = input("Enter Job location you are looking for(E.g. Pune) : ")
+    location = input(
+        "Enter Job location you are looking for(E.g. Pune) : ")
     location.replace(" ", "+")
     return location
 
 
-# url = "https://jobs.github.com/positions?utf8=✓&description=full+stack+developer&location=New+York"
-url = "https://jobs.github.com/positions?utf8=✓&description=" + getDescription() + "&location="+getLocation()
-req = requests.get(url)
-scrap = BeautifulSoup(req.text, "html.parser")
+class LinkedInScrapper:
+    def generateUrl(description, location):
 
-for i in scrap.find_all("tr", class_="job"):
-    print("\n\nPosition : " + i.td.h4.a.text)
-    print("Description Link : " + i.td.h4.a.attrs['href'])
-    print("Company Name : " + i.find("a", class_="company").text)
-    print("Company URL : " + i.find("a", class_="company").attrs['href'])
-    print("Job Type : "+i.find("strong").text)
-    print("Location : " + i.find("span", class_="location").text, "\n\n")
+        # Create URL
+        url = "https://www.linkedin.com/search/results/all/?keywords=" + description + "%20" + location + "&origin=GLOBAL_SEARCH_HEADER"
+        req = requests.get(url)
+        data = {
+            "req": req,
+            "url": url,
+        }
+        return data
+
+    def scraper(self):
+
+        description = getDescription()
+        location = getLocation()
+
+        data = self.generateUrl(self, description, location)
+
+        req = data['request']
+
+        scrap = BeautifulSoup(req.text, "html.parser")
+
+        for i in scrap.find_all("tr", class_="job"):
+            print("\n\nPosition : " + i.td.h4.a.text)
+            print("Description Link : " + i.td.h4.a.attrs['href'])
+            print("Company Name : " + i.find("a", class_="company").text)
+            print("Company URL : " + i.find("a",
+                                            class_="company").attrs['href'])
+            print("Job Type : "+i.find("strong").text)
+            print("Location : " + i.find("span", class_="location").text, "\n\n")
+
+
+class GitJobScrapper:
+
+    def generateUrl(description, location):
+
+        # Create URL
+        url = "https://jobs.github.com/positions?utf8=✓&description=" + \
+            description + "&location="+location
+        req = requests.get(url)
+        data = {
+            "req": req,
+            "url": url,
+        }
+        return data
+
+    def scraper(self):
+
+        description = getDescription()
+        location = getLocation()
+
+        data = self.generateUrl(self, description, location)
+
+        req = data['request']
+
+        scrap = BeautifulSoup(req.text, "html.parser")
+
+        for i in scrap.find_all("ul", class_="jobs-search-results__list"):
+            print("\n\nPosition : " + i.td.h4.a.text)
+            print("Description Link : " + i.td.h4.a.attrs['href'])
+            print("Company Name : " + i.find("a", class_="company").text)
+            print("Company URL : " + i.find("a",
+                                            class_="company").attrs['href'])
+            print("Job Type : "+i.find("strong").text)
+            print("Location : " + i.find("span", class_="location").text, "\n\n")
+
+
+def main():
+    
